7ce075a22415295407531633971f168ad9b045b2
kimauth <auth@chalmers.se>
2022-11-02 09:54:41 +0100

    move docstring

--- src/linear_elasticity.jl
+++ src/linear_elasticity.jl
@@ -1,14 +1,14 @@
+"""
+    LinearElasticity{dim}(;G, K) where dim
+
+Construct a `LinearElasticity` material based on the shear modulus `G` and the bulk modulus `K`.
+"""
 struct LinearElasticity{T,dim,M}
     G::T
     K::T
     Eᵉ::SymmetricTensor{4,dim,T,M}
 end
 
-"""
-    LinearElasticity{dim}(;G, K) where dim
-
-Construct a `LinearElasticity` material based on the shear modulus `G` and the bulk modulus `K`.
-"""
 function LinearElasticity{dim}(;G::Float64, K::Float64) where dim
     λ = K - 2/3*G
     δ(i,j) = i==j ? 1.0 : 0.0
